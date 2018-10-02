e075374b0825c1c06ccf94b73d8212e41bdc7f6b
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-10-02 09:25:13 -0400

    Updated from Brython Server: 10/2/2018 9:25:13 AM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Meg
+Credit: None
 
 Assignment:
 
@@ -10,3 +10,70 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+n=0
+m=0
+o=0
+sum1 = []
+encrypt = []
+string = ' '
+associations.find('c')
+associations[8]
+inpu1t= input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if inpu1t == 'e':
+    mess = input("Message: ")
+    key = input("Key: ")
+    list1=list(mess)
+    list2=list(key)
+    if len(list2)<len(list1):
+        diff =len(list1)-len(list2)
+        for n in range(0,diff):
+            list2.append(list2[n])
+            n+=1
+    tuples=list(zip(list1,list2))
+    for i in list1:
+        add1=associations.find(list1[m])+associations.find(list2[m])
+        if add1>len(associations):
+            add2=add1-len(associations)
+        else:
+            add2=add1
+        sum1.append(add2)
+        m+=1
+
+    for i in range(0,len(sum1)):
+        index=associations[sum1[o]]
+        encrypt.append(index)
+        o+=1
+    for i in encrypt:
+        string+=i
+    print(string)
+elif inpu1t == 'd':
+    mess = input("Message: ")
+    key = input("Key: ")
+    list1=list(mess)
+    list2=list(key)
+    if len(list2)<len(list1):
+        diff =len(list1)-len(list2)
+        for n in range(0,diff):
+            list2.append(list2[n])
+            n+=1
+    tuples=list(zip(list1,list2))
+    for i in list1:
+        add1=associations.find(list1[m])+associations.find(list2[m])
+        if add1>len(associations):
+            add2=add1-len(associations)
+        else:
+            add2=add1
+        sum1.append(add2)
+        m+=1
+
+    for i in range(0,len(sum1)):
+        index=associations[sum1[o]]
+        encrypt.append(index)
+        o+=1
+    for i in encrypt:
+        string+=i
+    print(string)
+elif inpu1t == 'q':
+    print("Goodbye!")
+else:
+    print("Did not understand command, try again.")
